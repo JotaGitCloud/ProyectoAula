fcc3c84a528e0a43db3fb406d8b24fc02f731a1f
JotaGitCloud <definitiveonlyplayer@gmail.com>
2025-10-29 22:01:44 -0500

    Delete P_Web directory

--- P_Web/JS/punto2.js
+++
@@ -1,8 +0,0 @@
-
-function mostrarUsuario(usuario) {
-    return `El usuario ${usuario.nombre} tiene el rol de ${usuario.rol}.`;
-}
-
-// Ejemplinho do uso mano kkkkkkkkkkkkkk
-let usuario = { nombre: "Ana", rol: "administrador" };
-console.log(mostrarUsuario(usuario));
--- P_Web/JS/punto8.js
+++
@@ -1,9 +0,0 @@
-let usuarios = [
-  { id: 1, nombre: "Juan" },
-  { id: 2, nombre: "María" },
-  { id: 3, nombre: "Pedro" }
-];
-
-let usuario = usuarios.find(u => u.id === 2) || {};
-
-console.log("Resultado:", usuario);
